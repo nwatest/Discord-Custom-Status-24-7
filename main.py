330759b8089ec13c583affbcad70801d86ac0222
nwatest <cxryzclout@gmail.com>
2024-06-03 07:42:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from keep_alive import keep_alive
 
 status = os.getenv("status") #online/dnd/idle
 
-custom_status = os.getenv("custom_status") #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
+custom_status = ""
 
 usertoken = os.getenv("token")
 if not usertoken:
